44900e5026dcd0873fe5328f7eb1b3ac1d01ab7e
Mitchkal <mitchellkalenda@gmail.com>
2024-03-14 15:38:37 -0400

    added lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,20 @@
+#!/usr/bn/python3
+"""
+Method to determine if all lockboxes are opened
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    comments
+    """
+    num_boxes = len(boxes)
+    unlocked_boxes = {0}
+    keys = set(boxes[0])
+
+    while keys:
+        key = keys.pop()
+        if key < num_boxes and key not in unlocked_boxes:
+            unlocked_boxes.add(key)
+            keys.update(boxes[key])
+    return len(unlocked_boxes) == num_boxes
