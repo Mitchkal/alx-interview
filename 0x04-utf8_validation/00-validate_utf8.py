479a069c0528299d3b5ff7cfebb473c2cc453ac5
Mitchkal <mitchellkalenda@gmail.com>
2024-04-04 17:19:22 -0400

    refactored to incorporate mask for checking most significant bit

--- 0x04-utf8_validation/00-validate_utf8.py
+++ 0x04-utf8_validation/00-validate_utf8.py
@@ -14,17 +14,22 @@ def validUTF8(data):
     num_bytes = 0
 
     for byte in data:
-        mask = 1 << 7
         if num_bytes == 0:
-            while mask & byte:
-                num_bytes += 1
-                mask = mask >> 1
-            if num_bytes == 0:
-                continue
-            if num_bytes == 1 or num_bytes > 4:
+            if byte >> 5 == 0b110:
+                num_bytes = 1
+            elif byte >> 4 == 0b1110:
+                num_bytes = 2
+            elif byte >> 3 == 0b11110:
+                num_bytes = 3
+            elif byte >> 7 != 0:
                 return False
         else:
-            if not (byte >> 6 == 0b10):
+            if byte >> 6 != 0b10:
                 return False
-        num_bytes -= 1
+
+            num_bytes -= 1
+
+            if num_bytes < 0:
+                return False
+
     return num_bytes == 0
